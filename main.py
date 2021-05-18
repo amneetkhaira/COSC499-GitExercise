db9d3120c3ce57d3a8fd0bbf330c3f9a9568ad7e
amneetkhaira <74328077+amneetkhaira@users.noreply.github.com>
2021-05-18 13:24:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import random
 #Generate a random number between 1 and 25
 randomNumber = random.randint(1,25)
+
+#Ask user to enter a number
+print('Guess the number between 1 and 25')
+#Assign user input to a variable
+guess = input()
+userGuess = int(guess)
+
